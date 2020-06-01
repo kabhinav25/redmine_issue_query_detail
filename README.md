a8467e658acd89ec98a54b7a220285b30da17867
kumar abhinav <kumar.abhinav@lookout.com>
2020-06-01 20:59:39 +0530

    Add permission check and update CSS

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# redmine_issue_query_detail
+# redmine_issue_query_detail (BETA)
+
+Redmine Plugin to show issue details on the Issue Query Page.
+
+<b>Compatibility</b> : Redmine 4.1+ versions.(I can develop for Redmine 3.x versions if people need it.)
+
+<b>Note</b>: The plugin is still in very early development stage. Active feedback will help in better development of the plugin. Use the github `Issues` to report any bug or feature requests.
+
+# Installation
+
+1. Copy the plugin directory into the $REDMINE_ROOT/plugins directory.
+2. Restart Redmine.
+
+# Usage
+
+The plugin can be used directly without needing to run any migrations or providing permissions.
+
+The issue query page will show two links to choose the view for query result:
+
+1. classic view - this is the current view used in issue query
+2. modern view - lists all the issues from the query result. Selecting an issue will load up its details.
--- app/controllers/issue_query_detail_controller.rb
+++ app/controllers/issue_query_detail_controller.rb
@@ -2,32 +2,27 @@ class IssueQueryDetailController < IssuesController
 
   skip_before_action :authorize
   before_action :find_issue
-  before_action :find_optional_project
   
   def issue_detail
-    @journals = @issue.visible_journals_with_index
-    @has_changesets = @issue.changesets.visible.preload(:repository, :user).exists?
-    @relations = @issue.relations.select {|r| r.other_issue(@issue) && r.other_issue(@issue).visible? }
+    if User.current.allowed_to?(:view_issues, @project) 
+      @journals = @issue.visible_journals_with_index
+      @has_changesets = @issue.changesets.visible.preload(:repository, :user).exists?
+      @relations = @issue.relations.select {|r| r.other_issue(@issue) && r.other_issue(@issue).visible? }
 
-    @journals.reverse! if User.current.wants_comments_in_reverse_order?
+      @journals.reverse! if User.current.wants_comments_in_reverse_order?
 
-    if User.current.allowed_to?(:view_time_entries, @project)
-      Issue.load_visible_spent_hours([@issue])
-      Issue.load_visible_total_spent_hours([@issue])
-    end
+      if User.current.allowed_to?(:view_time_entries, @project)
+        Issue.load_visible_spent_hours([@issue])
+        Issue.load_visible_total_spent_hours([@issue])
+      end
 
-    @allowed_statuses = @issue.new_statuses_allowed_to(User.current)
-    @priorities = IssuePriority.active
-    @time_entry = TimeEntry.new(:issue => @issue, :project => @issue.project)
-    @time_entries = @issue.time_entries.visible.preload(:activity, :user)
-    @relation = IssueRelation.new
-  end
-
-  def find_optional_project
-    if params[:project_id].present?
-      find_project(params[:project_id])
+      @allowed_statuses = @issue.new_statuses_allowed_to(User.current)
+      @priorities = IssuePriority.active
+      @time_entry = TimeEntry.new(:issue => @issue, :project => @issue.project)
+      @time_entries = @issue.time_entries.visible.preload(:activity, :user)
+      @relation = IssueRelation.new
+    else
+      render_404
     end
   end
-
-  private: find_optional_project
 end
